e6092210bee02e869eed0f6ef2685f4051bf95d1
parktaerim <parktaerim@localhost>
2024-03-26 16:47:16 +0900

    mac test

--- Gsimulator/src/main/java/com/mysite/sbb/MainController.java
+++ Gsimulator/src/main/java/com/mysite/sbb/MainController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class MainController {
+public class MainController {//test
 
 	@GetMapping("/sbb")
 	@ResponseBody
