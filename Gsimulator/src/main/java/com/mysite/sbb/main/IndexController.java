97795c2dbd41a127d439f562ccd775e1e35e4d5d
c <c@1.248.212.112>
2024-03-27 23:52:38 +0900

    modify(수정)미완

--- Gsimulator/src/main/java/com/mysite/sbb/main/IndexController.java
+++ Gsimulator/src/main/java/com/mysite/sbb/main/IndexController.java
@@ -4,12 +4,16 @@ import java.security.Principal;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.mysite.sbb.sugang.Sugang;
 
@@ -46,5 +50,21 @@ public class IndexController {
     showService.modify(sugang, semester, subjectName, credit, grade, subjectType, culture);
     return "redirect:/main/hello";  // 수정 후 리다이렉트할 뷰 이름
 }
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/main/modify/{id}")
+    public String mainModify(MainForm mainForm, @PathVariable("id") Integer id, Principal principal) {
+        Sugang sugang = this.showService.getSugangsForCurrentUser(id);
+        if(!sugang.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+        mainForm.setSemester(sugang.getSemester());
+        mainForm.setSubjectName(sugang.getSubjectName());
+        mainForm.setCredit(sugang.getCredit());
+        mainForm.setGrade(sugang.getGrade());
+        mainForm.setSubjectType(sugang.getSubjectType());
+        mainForm.setCulture(sugang.getCulture());
+        return "modify_form";
+    }
+    
     
 }
