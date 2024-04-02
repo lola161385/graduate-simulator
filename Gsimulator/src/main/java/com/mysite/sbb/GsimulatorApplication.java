90663287c734c0166d73fe01d9f01c638c6633bd
c <c@1.248.212.112>
2024-04-03 04:59:33 +0900

    배포버전으로 grdle 수정

--- Gsimulator/src/main/java/com/mysite/sbb/GsimulatorApplication.java
+++ Gsimulator/src/main/java/com/mysite/sbb/GsimulatorApplication.java
@@ -2,12 +2,20 @@ package com.mysite.sbb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GsimulatorApplication {
-
+public class GsimulatorApplication extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GsimulatorApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(GsimulatorApplication.class, args);
 	}
-
+	
+	
 }
